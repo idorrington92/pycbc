9dc61b7b188b745fad3fa3155856a9c157f9765c
Francesco Pannarale <pannarale@users.noreply.github.com>
2018-07-18 18:19:55 +0200

    Revert "Recent updates to coherent_mfilter branch"

--- pycbc/events/events.py
+++ pycbc/events/events.py
@@ -351,9 +351,12 @@ class EventManager(object):
     def add_template_events(self, columns, vectors):
         """ Add a vector indexed """
         # initialize with zeros - since vectors can be None, look for the
-        # longest one that isn't
+        # first one that isn't
         new_events = None
-        new_events = numpy.zeros(max([len(v) for v in vectors if v is not None]), dtype=self.event_dtype)
+        for v in vectors:
+            if v is not None:
+                new_events = numpy.zeros(len(v), dtype=self.event_dtype)
+                break
         # they shouldn't all be None
         assert new_events is not None
         new_events['template_id'] = self.template_index
@@ -581,9 +584,9 @@ class EventManagerCoherent(EventManager):
     def add_template_network_events(self, columns, vectors):
         """ Add a vector indexed """
         # initialize with zeros - since vectors can be None, look for the
-        # longest one that isn't
+        # first one that isn't
         new_events = None
-        new_events = numpy.zeros(max([len(v) for v in vectors if v is not None]), dtype=self.network_event_dtype)
+        new_events = numpy.zeros(max([len(v) for v in vectors]), dtype=self.network_event_dtype)
         # they shouldn't all be None
         assert new_events is not None
         new_events['template_id'] = self.template_index
